2fc8290143d9ab7ef388325be7e77bb861daf0f6
Philipp Neugebauer <ppn@philippneugebauer.net>
2018-06-06 15:25:08 +0200

    useless file calls removed

--- lib/iqvoc_oerk.rb
+++ lib/iqvoc_oerk.rb
@@ -2,12 +2,4 @@ require 'iqvoc/oerk/version'
 
 module IqvocOeRK
 
-  unless Iqvoc.const_defined?(:OeRK) && Iqvoc::OeRK.const_defined?(:Application)
-    require File.join(File.dirname(__FILE__), '../config/engine')
-  end
-
-  ActiveSupport.on_load(:after_iqvoc_config) do
-    require 'iqvoc'
-  end
-
 end
